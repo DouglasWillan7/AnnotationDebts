74f433cf8b754ddd512664a9fef0b5e514b601af
Douglas Willan Ribeiro Martins <douglas.martins@oletecnologia.com.br>
2019-05-13 17:27:07 -0300

    Removendo arquivos Kubernets

--- Source/AnnotationDebts.WebApi/_assembly.cs
+++
@@ -1,1 +0,0 @@
-﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "S4457:Split this method into two, one handling parameters check and the other handling the asynchronous code.")]
